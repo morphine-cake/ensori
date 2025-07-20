fdb5bae0e0e6f1a554f8ed09b850beb18dfcc065
Burak Basci <basci.burak@gmail.com>
2025-07-20 19:57:28 +0300

    Add Google Analytics tracking
    - Add gtag.js script to layout.tsx
    - Configure Google Analytics with tracking ID G-37FQN2MXTQ
    - Track page views and user interactions across the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -183,6 +183,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-37FQN2MXTQ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-37FQN2MXTQ');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
